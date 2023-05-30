b63b21f472b10a6c4dc2ccdc24864413b73b89cc
fabet <asilyk112@gmail.com>
2023-05-30 18:07:40 +0300

    Update buy dialog and add tariffs

---
+++ scripts/dialog.js
@@ -0,0 +1,18 @@
+let dialog = document.getElementById("myDialog");
+let buyButtons = document.getElementsByClassName("buy_button");
+let close = document.getElementsByClassName("close")[0];
+let saveButton = document.getElementById("saveButton");
+
+for (let button of buyButtons) {
+    button.onclick = function() {
+        dialog.style.display = "block";
+    }
+}
+
+close.onclick = function() {
+    dialog.style.display = "none";
+}
+
+saveButton.onclick = function() {
+    dialog.style.display = "none";
+}
